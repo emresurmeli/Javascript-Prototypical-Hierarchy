0d7fe1706b63f9d2704855e41e7863d49d6bb2f3
Emre Surmeli <s.emre.s.8@gmail.com>
2015-01-07 17:26:09 -0800

    object inheritance

---
+++ objectHeirarcy.js
@@ -0,0 +1,36 @@
+function Person(personality) {
+  this.personality = personality;
+}
+
+function SuperHero(power, weakness) {
+  this.power = power;
+  this.weakness = weakness;
+}
+SuperHero.prototype = new Person ("idealistic");
+
+function SuperVillian(power, weakness) {
+  this.power = power;
+  this.weakness = weakness;
+}
+SuperVillian.prototype = new Person ("evil");
+
+function RealisticHero(identity, job) {
+  this.identity = identity;
+  this.job = job;
+}
+RealisticHero.prototype = new SuperHero ("technology", "fatal wounds");
+
+function MagicalHero(magic, type) {
+  this.magic = magic;
+  this.type = type;
+}
+MagicalHero.prototype = new SuperHero ("sorcery", "old age");
+
+function EvilGenius(problem, IQ) {
+  this.problem = problem;
+  this.IQ = IQ;
+}
+EvilGenius.prototype = new SuperVillian ("lightning", "stone");
+
+var superman = new SuperHero ("flight", "kryptonite");
+var batman = new RealisticHero ("Bruce Wayne", "Millionare Playboy");
